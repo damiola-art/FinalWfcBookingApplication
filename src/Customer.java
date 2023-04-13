622112d20a1295076efb8a341495137cb0587e90
Damilola <59915965+damiola-art@users.noreply.github.com>
2023-04-13 14:14:14 +0100

    Created a Customer class

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,44 @@
 public class Customer {
+    private String firstname;
+
+    private String lastName;
+
+    private String email;
+
+
+    public Customer(String firstname, String lastName, String email){
+        this.firstname =firstname;
+        this.lastName = lastName;
+        this.email =email;
+    }
+
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
+
+
+
+
+
