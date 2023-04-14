a1d5368b8fd241f0b4936249c18a8cfbc0a727c7
Damilola <59915965+damiola-art@users.noreply.github.com>
2023-04-14 05:07:27 +0100

    Created Review class

---
+++ src/Review.java
@@ -0,0 +1,33 @@
+public class Review {
+    private Customer customer;
+    private int rating;
+
+    private String comments;
+
+    public Review(Customer customer, String lessonTypeOrDay, String comment, int rating) {
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+}
