26a78f810cf616e6bf9efe8e5a76323943c57ec8
Damilola <59915965+damiola-art@users.noreply.github.com>
2023-04-14 05:00:28 +0100

    updated the lesson class

--- src/Lesson.java
+++ src/Lesson.java
@@ -1,2 +1,68 @@
 public class Lesson {
+    private String type;
+    private int weekNumber;
+    private String customerName;
+    private double price;
+    private int capacity;
+
+    private Customer customer;
+
+    public Lesson(String type, int weekNumber, String customerName, double price, int capacity) {
+        this.setType(type);
+        this.setWeekNumber(weekNumber);
+        this.setCustomerName(customerName);
+        this.setPrice(price);
+        this.setCapacity(capacity);
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getWeekNumber() {
+        return weekNumber;
+    }
+
+    public void setWeekNumber(int weekNumber) {
+        this.weekNumber = weekNumber;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    // Getters and Setters
+    // ...
 }
