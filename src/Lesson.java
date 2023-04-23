4a4487b8c6232e88a55afcde5ec4f60f80a254af
Damilola <59915965+damiola-art@users.noreply.github.com>
2023-04-23 06:32:45 +0100

    created new instance

--- src/Lesson.java
+++ src/Lesson.java
@@ -1,19 +1,70 @@
+import java.time.LocalDate;
+
 public class Lesson {
     private String type;
     private int weekNumber;
     private String customerName;
     private double price;
-    private int capacity;
+    private int month; // new instance variable
 
+    private int noOfCustomers = 0;
+    private double totalPrice = 0;
     private Customer customer;
+    private LocalDate startDate;
 
-    public Lesson(String type, int weekNumber, Customer customer,String customerName, double price, int capacity) {
+    private Review review;
+
+    public Lesson(String type, int weekNumber, Customer customer, String customerName, double price, int month) {
         this.setType(type);
         this.setWeekNumber(weekNumber);
-        this.setCustomerName(customerName);
         this.setCustomer(customer);
+        this.setCustomerName(customerName);
         this.setPrice(price);
-        this.setCapacity(capacity);
+        this.setMonth(month);
+
+        this.startDate = startDate;
+    }
+
+    public int getNoOfCustomers() {
+        return noOfCustomers;
+    }
+
+    public void setNoOfCustomers(int noOfCustomers) {
+        this.noOfCustomers += noOfCustomers;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice += totalPrice;
+    }
+
+    public LocalDate getStartDate() { // new method to get start date
+        return this.startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) { // new method to set start date
+        this.startDate = startDate;
+    }
+    // getters and setters for the new variables
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+
+
+
+    // other existing getters and setters omitted for brevity...
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %s %s", getWeekNumber(), getType(), getPrice(), getCustomerName());
     }
 
     public String getType() {
@@ -48,14 +99,6 @@ public class Lesson {
         this.price = price;
     }
 
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-
     public Customer getCustomer() {
         return customer;
     }
@@ -64,12 +107,11 @@ public class Lesson {
         this.customer = customer;
     }
 
-    @Override
-    public String toString() {
-
-        return String.format("%s %s %s %s",weekNumber, type, price, customerName);
+    public Review getReview() {
+        return review;
     }
 
-    // Getters and Setters
-    // ...
+    public void setReview(Review review) {
+        this.review = review;
+    }
 }
