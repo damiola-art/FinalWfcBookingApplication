caa0191fdc9eff9ddb202cad6716b5edb4769d55
Damilola <59915965+damiola-art@users.noreply.github.com>
2023-04-24 20:21:22 +0100

    created test for 5 test based on the methods in lessonbookingsystem

--- src/LessonBookingSystemTest.java
+++ src/LessonBookingSystemTest.java
@@ -1,63 +1,78 @@
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.Assert.*;
 
 public class LessonBookingSystemTest {
+    LessonBookingSystem system;
+    Lesson lesson1;
+    Customer customer1;
+    LessonType fitnessType1;
 
-    @Before
-    public void setUp() throws Exception {
+
+    @BeforeEach
+    public void setUp() {
+        system = new LessonBookingSystem();
+        customer1 = new Customer("John", "Doe", "john.doe1@example.com");
+        fitnessType1 = new LessonType(10, "Bodysculpt");
+        lesson1 = new Lesson(fitnessType1, 1,1,"saturday", 1);
     }
 
-    @After
-    public void tearDown() throws Exception {
-    }
-
-    @Test
-    public void getBookings() {
-    }
-
-    @Test
-    public void getCustomers() {
-    }
-
-    @Test
-    public void generateID() {
-    }
-
-    @Test
-    public void RegisterNewBooking() {
+    @AfterEach
+    public void tearDown() {
+        system = null;
+        fitnessType1 = null;
+        lesson1 = null;
     }
 
     @Test
-    public void cancelBooking() {
-    }
-
-    @Test
-    public void changeBooking() {
-    }
-
-    @Test
-    public void attendLesson() {
-    }
-
-    @Test
-    public void getBooking() {
-    }
-
-    @Test
-    public void SignUp() {
-        LessonBookingSystem system = new LessonBookingSystem();
+    public void testSignUp() {
         Customer customer = system.signUp("John", "Doe", "john.doe@example.com");
-        assertNotNull(customer);
-        assertEquals("John", customer.getFirstname());
-        assertEquals("Doe", customer.getLastName());
-        assertEquals("john.doe@example.com", customer.getEmail());
-        assertEquals(0, customer.getBookings().size());
-        assertEquals(0, customer.getBookedLessons().size());
+        Customer result = system.getCustomer("john.doe@example.com");
+        assertEquals(result, customer);
     }
     @Test
-    public void getCustomer() {
+    public void testRegisterNewBooking() {
+        String bookingID = system.registerNewBooking(customer1, lesson1);
+        Booking booking = system.getBooking(bookingID);
+        assertEquals(customer1, booking.getCustomer());
+        assertEquals(lesson1, booking.getLesson());
+    }
+
+    @Test
+    public void testCancelBooking() {
+        String bookingID = system.registerNewBooking(customer1, lesson1);
+        Booking booking = system.getBooking(bookingID);
+        system.cancelBooking(booking);
+        assertEquals("cancelled", booking.getStatus());
+        assertFalse(customer1.getBookedLessons().contains(lesson1));
+    }
+
+    @Test
+    public void testChangeBooking() {
+        Lesson lesson2 = new Lesson(fitnessType1,2,2, "saturday", 1);
+        String bookingID = system.registerNewBooking(customer1, lesson1);
+        Booking booking = system.getBooking(bookingID);
+        system.changeBooking(booking, lesson2);
+        assertEquals("changed", booking.getStatus());
+        assertFalse(customer1.getBookedLessons().contains(lesson1));
+        assertTrue(customer1.getBookedLessons().contains(lesson2));
+        assertEquals(0, lesson1.getNumberOfBookings());
+        assertEquals(1, lesson2.getNumberOfBookings());
+    }
+
+    @Test
+    public void testAttendLesson() {
+        Review review = new Review("Great lesson!", 5);
+        String bookingID = system.registerNewBooking(customer1, lesson1);
+        Booking booking = system.getBooking(bookingID);
+        system.attendLesson(booking, review);
+        assertEquals("attended", booking.getStatus());
+        assertEquals(1, lesson1.getNumberOfAttendance());
+        assertTrue(lesson1.getReviews().contains(review));
+        assertEquals(review, booking.getReview());
     }
 }
